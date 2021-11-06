e028f813a6204dba86438806a3c6778528660d00
/Developer/ <mtfilmes04@gmail.com>
2021-11-06 02:00:51 +0000

    .

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
+<style>
+  img {
+  border: 1px solid
+  }
+  body { 
+    background-image: url(https://repository-images.githubusercontent.com/214671270/23f88b80-ed18-11e9-8a21-0a636b193f08)
+
+  }
+  hr.new5 {
+ 	border: 0;
+	border-top: 1px solid #094CFA;
+  border-inline-start: springgreen;
+}
+  button.sc {
+    background-color: whitesmoke;
+    border-color: rgb(0, 0, 0);
+  }
+  body.a {
+    background-color: whitesmoke;
+    width: 100%;
+    text-align: center;
+  }
+</style>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+
+  <HelloWorld msg="Site em construção" />
 </template>
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+
+<script>
+export default {
+  
+}
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Fruktur", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: black;
+  margin-top: 100px;
+  display: block;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  padding: 17;
+  margin-top: 270px;
+  background-color: whitesmoke;
+  opacity: 0.4;
+  border: 1px solid;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,15 @@
+
+
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+ 
 
-  <button @click="state.count++">count is: {{ state.count }}</button>
   <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    Sendo produzido para a vida de todos.
+</p>
 </template>
+
 
 <script setup>
 import { defineProps, reactive } from 'vue'
@@ -20,11 +18,16 @@ defineProps({
   msg: String
 })
 
-const state = reactive({ count: 0 })
+
+
+const state = reactive({ count: 1 })
 </script>
 
 <style scoped>
 a {
   color: #42b983;
+}
+p { 
+  color: black
 }
 </style>
