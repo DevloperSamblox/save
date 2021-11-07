a8835dd2eb63ae0ba7f07c9f69b8d4dfccc3364f
/Developer/ <mtfilmes04@gmail.com>
2021-11-07 02:55:32 +0000

    h

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-/**
- * https://vitejs.dev/config/
- * @type {import('vite').UserConfig}
- */
-export default {
+export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    hmr: {
-      port: 443,
-    }
-  }
-}
+  base: '',
+})
